0c82732c4b3f0d1eb42922dda1901f4c731dd1bb
MoonMinHyuk1 <moon981018@gmail.com>
2022-07-10 00:02:59 +0900

    section1-2

--- src/section1/translate2/Main.java
+++ src/section1/translate2/Main.java
@@ -7,17 +7,27 @@ public class Main {
     public String solution(String str) {
         String answer = "";
 
-        for(int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
+//        for(int i = 0; i < str.length(); i++) {
+//            char c = str.charAt(i);
+//
+//            if(c >= 'a' && c <= 'z') {
+//                c = Character.toUpperCase(c);
+//            }
+//            else if(c >= 'A' && c <= 'Z') {
+//                c = Character.toLowerCase(c);
+//            }
+//
+//            answer += c;
+//        }
 
-            if(c >= 'a' && c <= 'z') {
-                c = Character.toUpperCase(c);
+        for(char x : str.toCharArray()) {
+            if(Character.isLowerCase(x)) {
+                x = Character.toUpperCase(x);
             }
-            else if(c >= 'A' && c <= 'Z') {
-                c = Character.toLowerCase(c);
+            else if(Character.isUpperCase(x)) {
+                x = Character.toLowerCase(x);
             }
-
-            answer += c;
+            answer += x;
         }
 
         return answer;
