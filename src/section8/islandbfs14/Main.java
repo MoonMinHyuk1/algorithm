e9984c4742fdc40281da92580bce1051f133242e
MoonMinHyuk1 <moon981018@gmail.com>
2022-10-09 16:47:02 +0900

    section8-14

---
+++ src/section8/islandbfs14/Main.java
@@ -0,0 +1,66 @@
+package section8.islandbfs14;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Point {
+    public int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    static int[][] G, ch;
+    static int[] dx = {0, 0, 1, -1, 1, 1, -1, -1};
+    static int[] dy = {1, -1, 0, 0, 1, -1, 1, -1};
+    static int n, answer = 0;
+
+    public void BFS(int x, int y) {
+        Queue<Point> Q = new LinkedList<>();
+
+        Q.offer(new Point(x, y));
+        ch[x][y] = 1;
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for(int i = 0; i < len; i++) {
+                Point p = Q.poll();
+                for(int j = 0; j < 8; j++) {
+                    int nx = p.x + dx[j];
+                    int ny = p.y + dy[j];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                        if(G[nx][ny] == 1 && ch[nx][ny] == 0) {
+                            ch[nx][ny] = 1;
+                            Q.offer(new Point(nx, ny));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main A = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        G = new int[n][n];
+        ch = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                G[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(G[i][j] == 1 && ch[i][j] == 0) {
+                    A.BFS(i, j);
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
