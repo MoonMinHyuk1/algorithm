606d5566ca4b579fe338058d3eb9c1436b3d24ed
MoonMinHyuk1 <moon981018@gmail.com>
2022-12-14 15:32:35 +0900

    K번째 수

---
+++ python/inflearn/section2/2-2.py
@@ -0,0 +1,8 @@
+t = int(input())
+
+for i in range(t):
+    n, s, e, k = map(int, input().split())
+    L = list(map(int, input().split()))
+    L2 = L[s - 1:e + 1]
+    L2.sort()
+    print('#%d' %(i + 1) + ' %d' %L2[k - 1])
