98f01bf83e0a9779c059390d28e857c402caa160
MoonMinHyuk1 <moon981018@gmail.com>
2022-12-20 04:45:44 +0900

    역수열

---
+++ python/inflearn/section4/4-10.py
@@ -0,0 +1,15 @@
+n = int(input())
+l = list(map(int, input().split()))
+
+answer = []
+c = [0] * n
+for i in range(n):
+    for j in range(n):
+        if(l[j] == 0 and c[j] != 1):
+            answer.append(j + 1)
+            c[j] = 1
+            break
+        else:
+            l[j] -= 1
+print(*answer)
+        
