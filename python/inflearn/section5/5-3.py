0b37f881e40ed3641de1c73ba5c74faade5d2430
MoonMinHyuk1 <moon981018@gmail.com>
2022-12-22 18:22:29 +0900

    후위표기식 만들기

--- python/inflearn/section5/5-3.py
+++ python/inflearn/section5/5-3.py
@@ -1,2 +1,25 @@
 str = input()
 
+s = []
+answer = []
+for c in str:
+    if c.isdigit():
+        answer.append(c)
+    else:
+        if c == '(':
+            s.append(c)
+        if c == '+' or c == '-':
+            while s and s[-1] != '(':
+                answer.append(s.pop())
+            s.append(c)
+        if c == '*' or c == '/':
+            while s and (s[-1] == '*' or s[-1] == '/'):
+                answer.append(s.pop())
+            s.append(c)
+        if c == ')':
+            while s and s[-1] != '(':
+                answer.append(s.pop())
+            s.pop()
+while s:
+    answer.append(s.pop())
+print(*answer, sep = '')
