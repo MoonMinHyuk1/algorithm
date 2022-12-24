f77de8b02b15825f64e7e80a55afa0e427eeebf3
MoonMinHyuk1 <moon981018@gmail.com>
2022-12-25 00:18:40 +0900

    최소힙

---
+++ python/inflearn/section5/5-10.py
@@ -0,0 +1,14 @@
+import heapq
+
+h = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    elif n == 0:
+        if len(h) == 0:
+            print(-1)
+        else:
+            print(heapq.heappop(h))
+    else:
+        heapq.heappush(h, n)
