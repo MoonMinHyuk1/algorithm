1567ee7b09e6373e1be04e4455966885adc3aceb
MoonMinHyuk1 <moon981018@gmail.com>
2022-12-27 03:05:32 +0900

    부분집합 구하기

--- python/inflearn/section6/6-3.py
+++ python/inflearn/section6/6-3.py
@@ -10,3 +10,19 @@ def dfs(l, level, n):
 
 n = int(input())
 dfs([], 1, n)
+
+# 전역변수a
+# n = int(input())
+# l = list()
+
+# def dfs(level):
+#     if level == n + 1:
+#         if l:
+#             print(*l)
+#         return
+#     l.append(level)
+#     dfs(level + 1)
+#     l.remove(level)
+#     dfs(level + 1)
+
+# dfs(1)
