fe65efa0a078c464c91e2ca99031ebbf3ba7317a
MoonMinHyuk1 <moon981018@gmail.com>
2023-03-12 01:37:38 +0900

    포도주 시식

---
+++ python/baekjoon/dp/2156.py
@@ -0,0 +1,10 @@
+n = int(input())
+l = [int(input()) for _ in range(n)]
+dp = [0] * n
+dp[0] = l[0]
+if n > 1:
+    dp[1] = l[0] + l[1]
+
+for i in range(2, n):
+    dp[i] = max(dp[i - 3] + l[i - 1] + l[i], dp[i - 2] + l[i], dp[i - 1])
+print(dp[n - 1])
