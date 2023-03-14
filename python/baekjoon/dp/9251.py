a2adcbe9730684879a2839bd36e2f72511e199eb
MoonMinHyuk1 <moon981018@gmail.com>
2023-03-14 14:46:28 +0900

    LCS

---
+++ python/baekjoon/dp/9251.py
@@ -0,0 +1,12 @@
+str1 = input()
+str2 = input()
+dp = [0] * 1000
+
+for i in range(len(str1)):
+    cnt = 0
+    for j in range(len(str2)):
+        if dp[j] > cnt:
+            cnt = dp[j]
+        elif str1[i] == str2[j]:
+            dp[j] = cnt + 1
+print(max(dp))
