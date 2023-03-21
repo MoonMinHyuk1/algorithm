28bbeedadea44c81a255f2e793e6c3383cb2f355
MoonMinHyuk1 <moon981018@gmail.com>
2023-03-21 21:16:17 +0900

    연습장

---
+++ java_lan/programmers/Test.java
@@ -0,0 +1,21 @@
+package java_lan.programmers;
+
+import java.util.*;
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Test test = new Test();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        // String[] arr = {"a", "b", "c"};
+        // List<String> l = new ArrayList<>(Arrays.asList());
+        // l.stream().map(a -> map.put(a, new ArrayList<>()));
+
+        // int[] arr = {1, 2, 3};
+        // Arrays.stream(arr).forEach(n -> {
+            
+        // });
+    }
+}
