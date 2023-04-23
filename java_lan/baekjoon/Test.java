dcedf87c58aa74eff3ab21468d22c4de744cc12e
MoonMinHyuk1 <moon981018@gmail.com>
2023-04-23 19:15:03 +0900

    연습장

---
+++ java_lan/baekjoon/Test.java
@@ -0,0 +1,14 @@
+package java_lan.baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    }
+}
