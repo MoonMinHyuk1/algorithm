7d4762540a1bd62a628391f12585d7e643476bf1
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-13 02:10:13 +0900

    잃어버린 강아지

---
+++ java_lan/inflearn/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+inflearn.iml,b/5/b5ac6cec2369f9ecb18feab62cf7c13ed81c2b10
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+Z
+*src/simulation_implementation/Main1_1.java,2/f/2ff3002a5e8ff95c9d16f5d2ff251abcec90c970
--- java_lan/inflearn/src/simulation_implementation/Main1_3.java
+++ java_lan/inflearn/src/simulation_implementation/Main1_3.java
@@ -1,2 +1,80 @@
-package simulation_implementation;public class Main1_3 {
+package simulation_implementation;
+
+import java.util.*;
+
+public class Main1_3 {
+
+    public int solution(int[][] board){
+        int answer = 0;
+        int[] man = new int[2];
+        int[] dog = new int[2];
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int dMan = 0, dDog = 0;
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(board[i][j] == 2) {
+                    man[0] = i;
+                    man[1] = j;
+                } else if(board[i][j] == 3) {
+                    dog[0] = i;
+                    dog[1] = j;
+                }
+            }
+        }
+        while(!(man[0] == dog[0] && man[1] == dog[1])) {
+            answer++;
+            if(answer > 10000) {
+                answer = 0;
+                break;
+            }
+
+            int nx = man[0] + dx[dMan];
+            int ny = man[1] + dy[dMan];
+            if((nx >= 0 && nx < board.length && ny >= 0 && ny < board.length) && board[nx][ny] != 1) {
+                man[0] = nx;
+                man[1] = ny;
+            } else {
+                dMan = (++dMan) % 4;
+            }
+
+            nx = dog[0] + dx[dDog];
+            ny = dog[1] + dy[dDog];
+            if((nx >= 0 && nx < board.length && ny >= 0 && ny < board.length) && board[nx][ny] != 1) {
+                dog[0] = nx;
+                dog[1] = ny;
+            } else {
+                dDog = (++dDog) % 4;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main1_3 T = new Main1_3();
+        int[][] arr1 = {{0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 2, 0, 0},
+                {1, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0, 1},
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0}};
+        System.out.println(T.solution(arr1));
+        int[][] arr2 = {{1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 2, 1},
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 3}};
+        System.out.println(T.solution(arr2));
+    }
 }
