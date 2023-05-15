c36cb78513ed292706d1bd8eae06119657b6598f
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-16 04:26:35 +0900

    비밀번호

--- java_lan/inflearn/src/simulation_implementation/Main1_7.java
+++ java_lan/inflearn/src/simulation_implementation/Main1_7.java
@@ -1,5 +1,7 @@
 package simulation_implementation;
 
+import java.util.*;
+
 public class Main1_7 {
 
     public int solution(int[] keypad, String password){
@@ -53,11 +55,55 @@ public class Main1_7 {
         return answer;
     }
 
+    public int refactor(int[] keypad, String password) {
+        int answer = 0;
+        int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
+        int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
+        int[][] pad = new int[3][3];
+        int[][] dist = new int[10][10];
+        int[] pw = new int[password.length()];
+
+        for(int i = 0; i < pw.length; i++) {
+            pw[i] = password.charAt(i) - '0';
+        }
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                pad[i][j] = keypad[(i * 3) + j];
+            }
+        }
+        for(int i = 1; i < 10; i++) {
+            Arrays.fill(dist[i], 2);
+        }
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                int a = pad[i][j];
+                dist[a][a] = 0;
+                for(int k = 0; k < 8; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(nx >= 0 && nx < 3 && ny >= 0 && ny < 3) {
+                        int b = pad[nx][ny];
+                        dist[a][b] = 1;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < pw.length - 1; i++) {
+            answer += dist[pw[i]][pw[i + 1]];
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args){
         Main1_7 T = new Main1_7();
         System.out.println(T.solution(new int[]{2, 5, 3, 7, 1, 6, 4, 9, 8}, "7596218"));
         System.out.println(T.solution(new int[]{1, 5, 7, 3, 2, 8, 9, 4, 6}, "63855526592"));
         System.out.println(T.solution(new int[]{2, 9, 3, 7, 8, 6, 4, 5, 1}, "323254677"));
         System.out.println(T.solution(new int[]{1, 6, 7, 3, 8, 9, 4, 5, 2}, "3337772122"));
+        System.out.println(T.refactor(new int[]{2, 5, 3, 7, 1, 6, 4, 9, 8}, "7596218"));
+        System.out.println(T.refactor(new int[]{1, 5, 7, 3, 2, 8, 9, 4, 6}, "63855526592"));
+        System.out.println(T.refactor(new int[]{2, 9, 3, 7, 8, 6, 4, 5, 1}, "323254677"));
+        System.out.println(T.refactor(new int[]{1, 6, 7, 3, 8, 9, 4, 5, 2}, "3337772122"));
     }
 }
