e3116351684160b131155157b67c6cd0aba82715
MoonMinHyuk1 <moon981018@gmail.com>
2023-05-25 17:59:53 +0900

    최대 길이 연속수열

--- java_lan/inflearn/.idea/sonarlint/issuestore/index.pb
+++ java_lan/inflearn/.idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,14 @@ M
 src/hash_parsing/Main2_1.java,0/7/070a2929115b8e762a62cef77f25d59408c3ed4c
 M
 src/hash_parsing/Main2_2.java,7/6/762b68d203e2813541685b9850a19cb015d8e638
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+<
+inflearn.iml,b/5/b5ac6cec2369f9ecb18feab62cf7c13ed81c2b10
+M
+src/hash_parsing/Main2_3.java,a/9/a9a0cfa7b0872f85e58e677603d43ea0b230ba6e
+M
+src/hash_parsing/Main2_4.java,f/f/ffc11bb850a2977da63ad4f63af1b3da37575b57
+M
+src/hash_parsing/Main2_5.java,9/8/989340f5b2ba07db97a04a43e8ec80f12282d51e
--- java_lan/inflearn/src/data_structure/Main3_1.java
+++ java_lan/inflearn/src/data_structure/Main3_1.java
@@ -1,2 +1,37 @@
-package data_structure;public class Main3_1 {
+package data_structure;
+
+import java.util.*;
+
+public class Main3_1 {
+
+    public int solution(int[] nums){
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for(int n: nums) {
+            set.add(n);
+        }
+        for(int n: set) {
+            if(!set.contains(n - 1)) {
+                int cnt = 1;
+                int num = n + 1;
+                while(set.contains(num)) {
+                    cnt++;
+                    num++;
+                }
+                answer = Math.max(answer, cnt);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main3_1 T = new Main3_1();
+        System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
+        System.out.println(T.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+        System.out.println(T.solution(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
+        System.out.println(T.solution(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
+    }
 }
