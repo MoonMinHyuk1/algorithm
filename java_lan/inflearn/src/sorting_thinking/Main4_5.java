202e84433fea8bb1578c9aff912e3b52284e33ca
MoonMinHyuk1 <moon981018@gmail.com>
2023-06-01 02:45:11 +0900

    모임 장소

--- java_lan/inflearn/src/sorting_thinking/Main4_5.java
+++ java_lan/inflearn/src/sorting_thinking/Main4_5.java
@@ -31,10 +31,40 @@ public class Main4_5 {
         return answer;
     }
 
+    public int refactor(int[][] board) {
+        int answer = 0;
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                if(board[i][j] == 1) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        Collections.sort(col);
+        int x = row.get(row.size() / 2);
+        int y = col.get(col.size() / 2);
+        for(int n: row) {
+            answer += Math.abs(x - n);
+        }
+        for(int n: col) {
+            answer += Math.abs(y - n);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args){
         Main4_5 T = new Main4_5();
         System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
         System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
         System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
+
+        System.out.println(T.refactor(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
+        System.out.println(T.refactor(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+        System.out.println(T.refactor(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
     }
 }
