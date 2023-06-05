27a4e1c6ed727fb84a23f8c42c8bcabeedd23f52
MoonMinHyuk1 <moon981018@gmail.com>
2023-06-06 02:57:35 +0900

    이동 횟수

--- java_lan/inflearn/src/greedy/Main5_2.java
+++ java_lan/inflearn/src/greedy/Main5_2.java
@@ -1,2 +1,32 @@
-package greedy;public class Main5_2 {
+package greedy;
+
+import java.util.*;
+
+public class Main5_2 {
+
+    public int solution(int[] nums){
+        int answer = 0;
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        Arrays.sort(nums);
+        for(int n: nums) {
+            deque.offer(n);
+        }
+        while(!deque.isEmpty()) {
+            answer++;
+            int heavy = deque.pollLast();
+            while(!deque.isEmpty() && (deque.peekFirst() + heavy <= 5)) {
+                deque.pollFirst();
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main5_2 T = new Main5_2();
+        System.out.println(T.solution(new int[]{2, 5, 3, 4, 2, 3}));
+        System.out.println(T.solution(new int[]{2, 3, 4, 5}));
+        System.out.println(T.solution(new int[]{3, 3, 3, 3, 3}));
+    }
 }
