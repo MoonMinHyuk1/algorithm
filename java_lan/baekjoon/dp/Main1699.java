ffc44dccaf3a82037b38123968cc0949d1035d6a
MoonMinHyuk1 <moon981018@gmail.com>
2023-10-19 14:18:40 +0900

    제곱수의 합

---
+++ java_lan/baekjoon/dp/Main1699.java
@@ -0,0 +1,25 @@
+package java_lan.baekjoon.dp;
+
+import java.util.*;
+import java.io.*;
+
+public class Main1699 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i < dp.length; i++) {
+            int s = (int) Math.sqrt(i);
+            for(int j = s; j >= 1; j--) {
+                dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
