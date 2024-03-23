7ed4364b56c7bf4f4dd6b260018c0e4f49605f9e
MoonMinHyuk1 <moon981018@gmail.com>
2024-03-23 16:36:52 +0900

    플로이드

---
+++ java_lan/baekjoon/graph/Main11404.java
@@ -0,0 +1,47 @@
+package java_lan.baekjoon.graph;
+
+import java.util.*;
+import java.io.*;
+
+public class Main11404 {
+
+    public static void solution(int[][] arr) {
+        for(int k = 0; k < arr.length; k++) {
+            for(int i = 0; i < arr.length; i++) {
+                for(int j = 0; j < arr.length; j++) {
+                    if(k == i || k == j) continue;
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < arr.length; i++) {
+            Arrays.fill(arr[i], 10000000);
+            arr[i][i] = 0;
+        }
+        for(int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            arr[a][b] = Math.min(arr[a][b], c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        solution(arr);
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == 10000000) sb.append(0).append(" ");
+                else sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
